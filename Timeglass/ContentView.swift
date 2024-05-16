ecabb3de8e4b05855bb5af1f8b5d9b4c7879bd7b
saksham-s1ngh <saksham249singh@gmail.com>
2024-05-15 20:06:20 -0400

    Initial Commit

---
+++ Timeglass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timeglass
+//
+//  Created by Saksham Malhotra on 15/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timeglass/TimeglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeglassApp.swift
+//  Timeglass
+//
+//  Created by Saksham Malhotra on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
