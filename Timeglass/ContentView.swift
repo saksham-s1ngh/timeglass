9d573feaffb72ed6ebf818d7a2384c3e93a247fa
saksham-s1ngh <saksham249singh@gmail.com>
2024-06-05 19:59:09 -0400

    added the feature to reset timer on 180 degree flip
    the current feature took  A LOT of help from Youtube, StackOverFlow and ChatGPT since I don't have an idea how to utilise classes yet.
    Might need to come back to this one after I'm halfway through Swift.
    Resources used:
    1. https://www.youtube.com/watch?v=EiTpgJ5oMXg&t=2s
    2. https://developer.apple.com/documentation/coremotion/getting_raw_gyroscope_events
    3. https://chat.openai.com/

--- Timeglass/ContentView.swift
+++ Timeglass/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Combine
+import CoreMotion
 
 struct ContentView: View {
     
@@ -16,6 +17,7 @@ struct ContentView: View {
     @State var userTimerValue = ""
     @State var inputSetTime = 60
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @ObservedObject var motionManager = MotionManager()
     
     var body: some View {
         ZStack{
@@ -23,7 +25,7 @@ struct ContentView: View {
                 HStack {
                     Spacer()
                     FlipTimer(fTimer: $fTimer, timerRunning : $timerRunning, timer: timer)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, minHeight: 44, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .frame(minWidth: 100, minHeight: 44, alignment: .center)
                         .padding(10)
                         .background(
                             Color(#colorLiteral(red: 0.6642268896, green: 0.6642268896, blue: 0.6642268896, alpha: 1)))
@@ -40,7 +42,7 @@ struct ContentView: View {
                     .fill(Color.white)
                     .frame(maxWidth: .infinity, maxHeight: 500)
                     .padding()
-                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 0)
+                    .shadow(color: .black.opacity(0.3), radius: 10, x: 0.0, y: 0)
                 
                 Spacer()
                 
@@ -80,10 +82,10 @@ struct ContentView: View {
                             .fill(Color(#colorLiteral(red: 0.6642268896, green: 0.6642268896, blue: 0.6642268896, alpha: 1)))
                     )
                 }
-                .frame(minWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, minHeight: 44)
+                .frame(minWidth: 100, minHeight: 44)
                 .padding()
                 .padding(.trailing, 25)
-                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 0)
+                .shadow(color: .black.opacity(0.3), radius: 10, x: 0.0, y: 0)
                 
             }
         }
@@ -111,6 +113,10 @@ struct ContentView: View {
             .padding()
         }
         .background(Color.green)
+        .onAppear {
+            motionManager.resetCallback = self.resetTimer // Set the reset callback
+            motionManager.startGyroscope() // Start gyroscope updates
+        }
         
     }
     
@@ -122,6 +128,51 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+// feature I picked up from a youtube video and documentation, need to revise this more to improve.
+// currently a make-shift design which i also tinkered with on ChatGPT and Copilot to get a functioning feature
+class MotionManager: ObservableObject {
+    private var motionManager: CMMotionManager
+    @Published var isUpsideDown: Bool = false
+    private let threshold: Double = .pi // 180 degrees in radians
+    
+    var resetCallback: (() -> Void)?
+
+    init() {
+        self.motionManager = CMMotionManager()
+        self.startGyroscope()
+    }
+
+    func startGyroscope() {
+        if motionManager.isGyroAvailable {
+            motionManager.gyroUpdateInterval = 0.1
+            motionManager.startGyroUpdates(to: .main) { (data, error) in
+                guard let data = data else { return }
+                self.detectRotation(data: data)
+            }
+        }
+    }
+
+    private func detectRotation(data: CMGyroData) {
+        // z-axis rotation rate in radians per second
+        let rotationRateZ = data.rotationRate.z
+
+        // Check if the device is rotated more than 180 degrees
+        if abs(rotationRateZ) > self.threshold {
+            self.isUpsideDown = true
+            self.reset()
+        } else {
+            self.isUpsideDown = false
+        }
+    }
+
+    private func reset() {
+        resetCallback?()
+    }
+}
+
+// end of code-block that i picked up
 
 struct FlipTimer : View {
     
